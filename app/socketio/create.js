46a979a0eed6299d0e7af92e0aec5f9acc83fb91
Matti Jokitulppo <melonmanchan@gmail.com>
2016-12-27 00:40:01 +0200

    Add socket.io events, only allow one socket with certain name

--- app/socketio/create.js
+++ app/socketio/create.js
@@ -1,23 +1,50 @@
 import socketio from 'socket.io';
+
 import { log, LOG_TYPES } from '../utils';
+import EVENT_TYPES        from './eventtypes';
 
 const sockets = [];
 
 function createSocketIO(server) {
     const io = socketio(server);
 
-    io.on('connection', (socket) => {
-        log('New socket with id: ' + socket.id);
+    io.on(EVENT_TYPES.NEW_CONNECTION, (socket) => {
+        const username = socket.handshake.query.username;
+
+        if (!username) {
+            socket.disconnect(true);
+            return;
+        }
+
+        const existing = sockets.find(s => {
+            return s.user.username === username
+        });
+
+        if (existing) {
+            log('Name is taken!', LOG_TYPES.warn);
+            socket.emit(EVENT_TYPES.NAME_TAKEN, {});
+            socket.disconnect(true);
+            return;
+        }
+
+        log('New client connected: ' + username);
+
+        socket.on(EVENT_TYPES.DISCONNECT, () => {
+            log(`Socket disconnected with id ${socket.id}`, LOG_TYPES.WARN);
+
+            const i = sockets.indexOf(socket);
+
+            if (i === -1) {
+                return;
+            }
+
+            sockets.splice(i, 1);
+        });
+
+        socket.user = { username };
         sockets.push(socket);
 
-        socket.on('disconnect', () => {
-            log('Socket disconnect with id' + socket.id, LOG_TYPES.WARNING);
-            const i = connections.indexOf(socket);
-            if (i === -1) return;
-            const user = socket.user;
-
-            connections.splice(i, 1);
-        });
+        socket.emit(EVENT_TYPES.LOGGED_IN, {});
     });
 }
 
---
+++ app/socketio/eventtypes.js
@@ -0,0 +1,7 @@
+export default {
+    NEW_CONNECTION : 'connection',
+    DISCONNECT     : 'disconnect',
+
+    NAME_TAKEN     : 'name_taken',
+    LOGGED_IN      : 'logged_in',
+};
